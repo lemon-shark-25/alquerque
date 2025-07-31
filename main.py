3b35d91dd85683437e7be783e07cc3372c8ef5ca
lemon-shark <196343170+lemon-shark-25@users.noreply.github.com>
2025-07-31 12:24:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import pygame
 from alquerque import WIDTH, HEIGHT
+from alquerque.board import Board
+
+# Local constants
 FPS = 60
 
 pygame.init()
-pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Alquerque")
 
 def main():
     running = True
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
+
+    board = Board()
+    board.create_board()
 
     while running:
+        clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        # Rendering
+        board.draw(WIN)
+
+        pygame.display.flip()
 
     pygame.quit()
 
